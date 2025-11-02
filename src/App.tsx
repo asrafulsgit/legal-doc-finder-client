a560ef951f2ca7aacb54bd731a1eae96ab9e4a73
sourob2356 <sourob2356@gmail.com>
2025-11-02 20:21:17 +0600

    add Navbar component and add Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar"
 
 function App() {
   
 
   return (
     <>
-    <h1>hello word</h1>
+        <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 left-0 
+    right-0 z-50 bg-white shadow-sm py-4 pl-10">
+          <h1 className="font-bold text-2xl">LegalDoc<span className="text-blue-700">Finder</span> </h1>
+    </nav>
+  )
+}
+
+export default Navbar
